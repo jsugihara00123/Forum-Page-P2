4057af07aba981a00f383b53405397bbe7c3a932
Jonathan Sugihara <jonathansugihara@Jonathans-MacBook-Pro.local>
2022-10-04 19:06:16 -0600

    api controllers

---
+++ controllers/api/topicRoutes.js
@@ -0,0 +1,38 @@
+const router = require('express').Router();
+const { Topic } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.post('/', withAuth, async (req, res) => {
+  try {
+    const newTopic = await Topic.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newTopic);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const topicData = await Topic.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!topicData) {
+      res.status(404).json({ message: 'No topic found with this id!' });
+      return;
+    }
+
+    res.status(200).json(topicData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,3 +1,81 @@
 const router = require('express').Router();
-const { Project, User } = require('../models');
+const { Topic, User } = require('../models');
 const withAuth = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+  try {
+    // Get all topics and JOIN with user data
+    const topicData = await Topic.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+        // Serialize data so the template thats readable 
+        const topics = topicData.map((topic) => topic.get({ plain: true }));
+
+        // Pass serialized data and session flag into template
+        res.render('homepage', { 
+          topics, 
+          logged_in: req.session.logged_in 
+        });
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    });
+
+    router.get('/topic/:id', async (req, res) => {
+      try {
+        const topicData = await Topic.findByPk(req.params.id, {
+          include: [
+            {
+              model: User,
+              attributes: ['name'],
+            },
+          ],
+        });
+    
+        const topic = topicData.get({ plain: true });
+    
+        res.render('topic', {
+          ...topic,
+          logged_in: req.session.logged_in
+        });
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    });
+
+    router.get('/profile', withAuth, async (req, res) => {
+      try {
+        // Find the logged in user based on the session ID
+        const userData = await User.findByPk(req.session.user_id, {
+          attributes: { exclude: ['password'] },
+          include: [{ model: Topic }],
+        });
+    
+        const user = userData.get({ plain: true });
+    
+        res.render('profile', {
+          ...user,
+          logged_in: true
+        });
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    });
+
+    router.get('/login', (req, res) => {
+      // If the user is already logged in, redirect the request to another route
+      if (req.session.logged_in) {
+        res.redirect('/profile');
+        return;
+      }
+    
+      res.render('login');
+    });
+    
+    module.exports = router;
